e4b84792f163807305b493a230201f739f067442
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-17 13:27:43 +0100

    refactor(views): use stores

--- views/alerts.py
+++ views/alerts.py
@@ -1,21 +1,29 @@
-import json
 from flask import Blueprint, render_template, request
 from models.alert import Alert
+from models.store import Store
+from models.item import Item
 
 alert_blueprint = Blueprint('alerts', __name__)
+
 
 @alert_blueprint.route('/')
 def index():
     alerts = Alert.all()
     return render_template('alerts/index.html', alerts=alerts)
 
+
 @alert_blueprint.route('/new', methods=["GET", "POST"])
 def new_alert():
     if request.method == "POST":
 
-        item_id = request.form['item_id']
-        price_limit = request.form['price_limit']
+        item_url = request.form['item_url']
+        price_limit = float(request.form['price_limit'])
 
-        Alert(item_id, price_limit).save_to_db()
+        store = Store.find_by_url(item_url)
+        item = Item(item_url, store.tag_name, store.query)
+
+        item.save_to_db()
+
+        Alert(item._id, price_limit).save_to_db()
 
     return render_template('alerts/new_alert.html')
