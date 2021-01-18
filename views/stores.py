83058288b6813b3fb2a1119125b7c6208139d677
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-18 11:41:58 +0100

    feat(stores): add edit store

--- views/stores.py
+++ views/stores.py
@@ -27,3 +27,20 @@ def new_store():
         return redirect(url_for('.index'))
 
     return render_template('stores/new_store.html')
+
+
+@store_blueprint.route('/edit/<string:store_id>', methods=["GET", "POST"])
+def edit_store(store_id):
+    store = Store.get_by_id(store_id)
+
+    if request.method == "POST":
+        store.name = request.form['name']
+        store.url_prefix = request.form['url_prefix']
+        store.tag_name = request.form['tag_name']
+        store.query = json.loads(request.form['query'])
+
+        store.save_to_db()
+
+        return redirect(url_for('.index'))
+
+    return render_template('stores/edit_store.html', store=store, query=json.dumps(store.query))
