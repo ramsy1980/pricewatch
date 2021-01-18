d1a2dd3400b2c6e55b3752c69983c09c31ead938
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-18 11:11:49 +0100

    feat(alerts): add update alert

--- views/alerts.py
+++ views/alerts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from models.alert import Alert
 from models.store import Store
 from models.item import Item
@@ -15,7 +15,6 @@ def index():
 @alert_blueprint.route('/new', methods=["GET", "POST"])
 def new_alert():
     if request.method == "POST":
-
         alert_name = request.form['name']
         item_url = request.form['item_url']
         price_limit = float(request.form['price_limit'])
@@ -28,4 +27,20 @@ def new_alert():
 
         Alert(alert_name, item._id, price_limit).save_to_db()
 
+        return redirect(url_for('.index'))
+
     return render_template('alerts/new_alert.html')
+
+
+@alert_blueprint.route('/edit/<string:alert_id>', methods=["GET", "POST"])
+def edit_alert(alert_id):
+    alert = Alert.get_by_id(alert_id)
+
+    if request.method == "POST":
+        price_limit = float(request.form['price_limit'])
+        alert.price_limit = price_limit
+        alert.save_to_db()
+
+        return redirect(url_for('.index'))
+
+    return render_template('alerts/edit_alert.html', alert=alert)
