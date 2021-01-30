42d253723dd6f0a753a17e360c78755e6b41b71f
Ramsy de Vos <ramsy@ramsy.dev>
2021-01-30 18:02:20 +0100

    feat(credits): sort payments and notifications

--- src/views/credits.py
+++ src/views/credits.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, flash, url_for
 from src.models import User, requires_login, Payment, Notification
 from src.common import DisplayFlashMessages
+from datetime import datetime
 
 credit_blueprint = Blueprint('credits', __name__)
 
@@ -12,14 +13,22 @@ def index():
     payments = Payment.find_many_by("user_id", user.id)
     notifications = Notification.find_many_by("user_id", user.id)
 
+    sorted_payments = sorted(payments, key=lambda x: x.created, reverse=True)
+    sorted_notifications = sorted(notifications, key=lambda x: x.created, reverse=True)
+
     if user.is_email_verified() and user.phone_number == "":
         DisplayFlashMessages.phone_number_not_verified()
 
-    return render_template('/credits/index.html', user=user, payments=payments, notifications=notifications, active="credits")
+    return render_template(
+        '/credits/index.html',
+        user=user,
+        payments=sorted_payments,
+        notifications=sorted_notifications,
+        active="credits"
+    )
 
 
 @credit_blueprint.route('/add')
 @requires_login
 def add_credits():
     return render_template('/credits/add.html')
-
